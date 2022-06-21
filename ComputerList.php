7811a5055111ebb1ac71d228dc52d4818af10ca3
Joserra2304 <cydogamer@gmail.com>
2022-06-21 03:32:22 +0200

    Añadiendo Front-end

--- ComputerList.php
+++ ComputerList.php
@@ -1,7 +1,45 @@
-<?php
-$conection = "";
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/css.css">
+</head>
+<body>
 
-include "css/css.css";
+<?php
+set_include_path('C:\Users\Joserra\Desktop\MisCursos\DAM\SemanaPracticas2\WEB_JFS_2DAM\localhost\OrderList.php');
+$conection = "";
+$name="";
+
 include "conection/conection.php";
+$query="select * from computers";
+$result= mysqli_query($conection,$query);
+?>
+<div class = "title">LISTA DE ORDENADORES</div>
+<?php
+while ($field = mysqli_fetch_object($result)) {
+    echo '
+            <div class = "table">
+                <div class="field">
+                <p>'.$field->computer_id.'</p>
+                </div>
+                <div class="field">
+                <p>'.$field->user_id.'</p>
+                 </div>
+                 <div class="field">
+                <p>'.$field->brand.'</p>
+                 </div>
+                  <div class="field">
+                <p>'.$field->model.'</p>
+                 </div>
+                  <div class="field">
+                <p>'.$field->ram.'</p>
+                 </div>
+                  <div class="field">
+                <p>'.$field->repaired.'</p>
+                 </div>
+            </div>';
+}
+
 
 ?>
+
--- OrderList.php
+++ OrderList.php
@@ -1,7 +1,40 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/css.css">
+</head>
+<body>
+
 <?php
+set_include_path('C:\Users\Joserra\Desktop\MisCursos\DAM\SemanaPracticas2\WEB_JFS_2DAM\localhost\OrderList.php');
 $conection = "";
+$name="";
 
-include "css/css.css";
 include "conection/conection.php";
-
+$query="select * from orders";
+$result= mysqli_query($conection,$query);
 ?>
+<div class = "title">LISTA DE ORDENES</div>
+<?php
+        while ($field = mysqli_fetch_object($result)) {
+        echo '
+            <div class = "table">
+                <div class="field">
+                <p>'.$field->order_id.'</p>
+                </div>
+                <div class="field">
+                <p>'.$field->computer_id.'</p>
+                 </div>
+                 <div class="field">
+                <p>'.$field->date.'</p>
+                 </div>
+                  <div class="field">
+                <p>'.$field->description.'</p>
+                 </div>
+            </div>';
+        }
+?>
+
+
+
+
